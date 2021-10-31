56b91df394ea06176e051a50e336de07b8f93145
David <david87124@gmail.com>
2021-10-31 23:33:47 +0800

    test

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
     return res.send("Express")
 })
 
+app.get('/test', (req, res) => {
+    return res.send("Express test")
+})
+
 app.listen(3000, () => console.log("Server run on port 3000"))
